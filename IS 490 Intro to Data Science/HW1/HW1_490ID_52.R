39910680d2f98cb8e2fa578724d95bf5a913b6ed
roger <pingche2@illinois.edu>
2017-09-17 23:55:10 -0500

    Making the finishing touches on the HW1 R files, created a R markdown in html, still working on it to finish it.

--- IS 490 Intro to Data Science/HW1/HW1_490ID_52.R
+++ IS 490 Intro to Data Science/HW1/HW1_490ID_52.R
@@ -25,12 +25,24 @@ nrow(diamonds) # 53940 rows
 # to answer the questions.
 
 #(3). Show the summary or the structure of this dataset.
-summary(diamonds) # summary : 
+summary(diamonds) # summary :  !!!!!!!!
 
 dim(diamonds)  # Rows: 53940, Columns: 10
 
-#(4). List the categorical variables in this dataset.
+#(4). List the categorical variables in this dataset. !!!
 objects(diamonds)
+names(diamonds)
+class(diamonds$carat)
+class(diamonds$cut)
+class(diamonds$color)
+class(diamonds$clarity)
+class(diamonds$depth)
+class(diamonds$table)
+class(diamonds$price)
+class(diamonds$x)
+class(diamonds$y)
+class(diamonds$z)
+# Ans: cut, color, clarity
 
 #(5). What was the highest price of the diamonds ?
 max(diamonds$price) # 18823
@@ -59,7 +71,6 @@ abs(SI2.price.avg - IF.price.avg)  # 2198.189
 aggr = aggregate(diamonds$price, by = list(diamonds$clarity), FUN=mean)
 class(aggr) # data.frame
 abs(aggr$x[aggr$Group.1 == "IF"] - aggr$x[aggr$Group.1 == "SI2"]) # 2198.189
-
 #(10). Total depth percentage is represented as the depth divided by
 #      the mean of the length and width of the diamond, 
 # (11).
@@ -72,21 +83,28 @@ z/mean(x, y)
 ### Error message here
 #         Error: object 'z' not found
 ### Explanation here
-# sdfghjkl;kjhgfdsdfghjk@!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# "Because the z variable was not declared to be anything before running the expression, so running this expression would result
+# R unable to find the z object,thus causing an error."
 
 diamonds$z/(mean(diamonds$x, diamonds$y))
 ### Error message here
         #Error in mean.default(diamonds$x, diamonds$y) : 
         #'trim' must be numeric of length one
 ### Explanation here
-#       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# "Because the mean function has several parameters, and only 1 default parameter.
+# The expression using the mean(diamonds$x, diamonds$y) has the diamonds$y object match
+# the 'trim' parameter, which takes a fraction (0 to 0.5) of observations to be trimmed form each end of the first argument in the mean function
+# before the mean is computed. yet the parameter 'diamonds$y' that we passed in the mean function has a length of 53940 numeric values,
+# thus causing an error that the 'trim' parameter must be numberic of length one."
 
 diamonds$z/(rowMeans(diamonds$x, diamonds$y))
 ### Error message here
         #Error in rowMeans(diamonds$x, diamonds$y) : 
         #  'x' must be an array of at least two dimensions
 ### Explanation here
-#       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#"The 'rowMeans' function has to take in an array of two or more dimensions or a numeric data frame.
+# And the expression gave it diamonds$x as the first 'x' argument, which is only a 1 dimension numeric vector,
+# thus and error occurs because the first argument must have more than 1 dimension."
 
 #(12).Study the following code about how to do the compution 
 # calculation that we want for the previous question.
@@ -94,11 +112,11 @@ Depth <- (diamonds$z)/rowMeans(cbind(diamonds$x, diamonds$y))
 
 #(13). Can you get the same result without using function?
 #	      Given an expression to it.  Name the values Depth1
-Depth1 = diamonds$z/ (diamonds$y + diamonds$x/((length(diamonds$y) + length(diamonds$x))))
+Depth1 = diamonds$z / ((diamonds$y + diamonds$x) / 2)
 
 #(14) What did you get from 
 all.equal(Depth, Depth1)
-#         "Mean relative difference: 0.004049661"
+# True
 
 # 2. Run the following code to make a plot.
 # (don't worry right now about what this code is doing)
@@ -108,7 +126,7 @@ plot(diamonds[1:1000,]$carat, diamonds[1:1000,]$price, xlab = "Carat", ylab = "P
 # (1) Use the Zoom button in the Plots window to enlarge the plot.
 # Resize the plot so that it is long and short, so it is easier to read.
 # Include this plot in the homework your turn in.
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!aadsasdasdsaaadadsdsaads~~~~~~=====!!!!!!
+
 # (2) Make an interesting observation about the relation between
 #     Carat and Price based on this plot 
 # (something that you couldn't see with the calculations so far.)
@@ -127,12 +145,13 @@ plot(diamonds[1:1000,]$carat, diamonds[1:1000,]$price, xlab = "Carat", ylab = "P
 
 ### Your answer goes here
 
-#  "I would like to know from the data, how likely and what is the probability of a diamond 
-#   having a higher price with a lower weight in weights larger than 0.5 carat be?"
-!!!!!!!!!!!!!
-#   " To answer this question, I would have to count the fraction of the number of diamonds with a higher price per carat
-#   " than those that are heavier than itself."
-!!!!!!!!!!!!
+#  "Common intuition would suggest that a diamond with a higher weight in carat 
+#   would have a higher pricing than those that have less weight in carat, does that hold true?"
+
+#   " To answer this question, I would have to count the portions of diamonds with a higher price per carat
+#     than those that are heavier than itself in the dataset. If the portion is large, then it means that for diamonds,
+#     carat and price isn't highly correlated, and that heavier diamonds do not necessarily mean they have a higer price." 
+
   
 # For the remainder of this assignment we will work with 
 # one of the random number generators in R.
@@ -181,10 +200,11 @@ length(normsamps[normsamps > 8]) / length(normsamps) # 0.07
 
 
 # What value do you expect? 
- # expected with a 50 % + 34% + 11% making it close to 0.95
+ # expected value from the previous question : 0.07
 # What value did you get? 
-pnorm(8, mean = 5, sd = 2)
-#     0.9331928
+1 - pnorm(8, mean = 5, sd = 2)
+#     0.0668072
 # Why might they be different?
-# computer calculations are way more precise than a human mind !!!
+# Because the first one only had a sample of 100 which is a really small sample size, unable to precisely calculate what it may actually be.
+# As for the R program, it uses a huge sample size to make sure the computation precise as it can be.
 
