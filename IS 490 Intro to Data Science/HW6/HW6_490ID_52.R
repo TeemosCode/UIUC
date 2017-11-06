1d9d93e5919b2e73f9820e17beeb29aaf30562c1
roger <pingche2@illinois.edu>
2017-11-05 23:29:59 -0600

    Regular expression in Data science

--- IS 490 Intro to Data Science/HW6/HW6_490ID_52.R
+++ IS 490 Intro to Data Science/HW6/HW6_490ID_52.R
@@ -30,14 +30,14 @@ X_bar = mean(test_sample)
 # > X_bar
 # [1] 7.78
 
-##(3) What is the confidence interval for X_bar? (Recall HW4 Q5) (1 pts) ******
-SE = sd(test_sample)
+##(3) What is the confidence interval for X_bar? (Recall HW4 Q5) (1 pts) *********
+SE = sd(test_sample)/sqrt(100)
 # > SE
-# [1] 1.382283
+# [1] 0.1382283
 confidence_interval = c(X_bar - SE, X_bar + SE)
 # > confidence_interval
-# [1] 6.397717 9.162283
-# Ans: Confidence Interval = [mean +- SE(which equals SD)] = [7.78 +- 1.382283] = [6.397717, 9.162283]
+# [1] 7.641772 7.918228
+# Ans: Confidence Interval = [mean +- SE] = [7.78 +- 0.1382283] = [7.641772, 7.918228]
 
 #(c) Now use the simulation to estimate the distribution of X_bar and create confidence intervals for it using that distribution.
 ##(1) Form a set of X_bar by repeating B = 1000 times the individual experiment. You may want to create a matrix to save those values.(1 pts)
@@ -79,7 +79,6 @@ confidence_interval = c(X_bar - 1.96 * SE, X_bar + 1.96 * SE)
 # generalize (c) into a function, and vary inputs of sample size and B as we did above. (2 pts)
 
 MC_sample <- function(sample_size, B){
-  
   #code here
   L = list()
   for(index in 1:B){
@@ -93,29 +92,82 @@ MC_sample <- function(sample_size, B){
 
 #(e).Plot your CI limits to compare the effect of changing the sample size and 
 # changing the number of simulation replications B (2 plots). What do you conclude? (3 pts)
+for(i in 1:5){
+  sz = sample(50:250 , 1, replace = T)
+  B = sample(900: 1500, 1 , replace = T)
+  if(i == 1){
+    CIs = matrix(MC_sample(sz,B), ncol = 2)
+  }else{
+    CIs = rbind(CIs, MC_sample(sz,B))
+  }
+}
+plot(CIs)
+# Ans:
+
 
 ################################### Part 2: Regular Expression(Regular Expression or R) (22 pts) ##############################################
 
 #(a) Write down a general regular expression to match the following:(General Regular Expression)
-
 ##(1) Words/tokens only have 's' as start or end. For example, stats, specifies, start, ends etc.(1 pts)
+# Ans: ^s?[a-z]*s|s[a-z]*s?$
 
 ##(2) A string with the format <a>text</a>, <b>xxx</b> etc.(1 pts)
+# Ans: <[a-z]>.*<\/[a-z]>
 
 ##(3) An email address that ends with .com, .edu, .net, .org, or .gov(1 pts)
+# Ans: [a-zA-Z0-9._-]+@[a-zA-Z0-9._+-]+\.(com|edu|net|org|gov)
 
 #(b) Carry out the following exercises on the State of the Union Speeches dataset(available in moodle, stateoftheunion1790-2012.txt). (R)
 # (Suggestion: check the .txt data before coding the solutions and also lapply could be really helpful)
+setwd("./Desktop/gitHub-UIUC/IS 490 Intro to Data Science/HW6/")
 
 ##(1) Use readLines() to read in the speeches where the return value is: character vector with one element/character string per line in the file save as su_data (1 pts)
-
+su_data = readLines("stateoftheunion1790-2012.txt")
 ##(2) Use regular expressions and R to return the number of speeches in the dataset, and the number of presidents that gave speeches.(2 pts)
+#####  number of speeches  #####
+regSpeech = "[A-Z][a-z\ ]+,\ [A-Z][A-Za-z\ ]+,\ [A-Z][A-Za-z\ 0-9]+,\ [0-9]+"
+length(grep(regSpeech, su_data))
+### ANS: 221 ###
+# > length(grep(regSpeech, su_data))
+# [1] 221
+#####  number of presidents that gave speeches  #####
+speeches = su_data[grep(regSpeech, su_data)]
+speechInfo = strsplit(speeches, ',')
+
+SpeechInfoExtract = function(lst, pos, func){
+  vec = c()
+  for(i in 1:length(lst)){
+    vec[i] = lst[[i]][pos]
+  }
+  return(func(vec))
+}
+
+pres = SpeechInfoExtract(speechInfo, 1, unique)
+length(pres)
+### ANS: 41 ###
+# > length(pres)
+# [1] 41
 
 ##(3) Use regular expressions to identify the date of the speech (save as su_date), extract the name of the speaker (save as su_speaker)
 ## extract the year (save as su_year) and the month of the date (save as su_month) (4 pts)
+su_date = SpeechInfoExtract(speechInfo, 3, c)
+su_speaker = SpeechInfoExtract(speechInfo, 1, c)
+su_year = SpeechInfoExtract(speechInfo, 4, c)
+su_month = SpeechInfoExtract(strsplit(su_date, ' '), 2, unique)
 
 ##(4) Merge the lines up into a list named su_speeches. Each element of the list is a character vector containing one speech. 
 ## The length of su_speeches should be the number of speeches in the data. Check: does the length of your list match your answer above? (3 pts)
+su_speeches = c()
+try = paste(su_data, collapse = '')
+try_str = as.character(try)
+splits = strsplit(try_str, "***", fixed = T)
+for(i in 2:223){
+  su_speeches[i-1] = splits[[1]][i] 
+}
+length(su_speeches)
+# > length(su_speeches)
+# [1] 222
+# Ans: 222, it is longer than it by 1.
 
 ##(5) Eliminate apostrophes, numbers, and the phrase: (Applause.) and make all the characters lowercase for each element in su_speeches. (3 pts)
 
@@ -126,3 +178,7 @@ MC_sample <- function(sample_size, B){
 
 ##(8) Carry out some exploratory analysis of the data and term frequencies. For example, find the number of sentences, extract the
 ## long words, and the political party. Plot and interpret the term frequencies. What are your observations? (3 pts)
+  
+  
+
+
