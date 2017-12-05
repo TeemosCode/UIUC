ce0fd7d226dde81e87648d039ac966aedeadd24c
Roger Ho <pingche2@illinois.edu>
2017-12-05 02:56:31 -0600

    finished the shell script to getting the information needed regarding the IMDB html data

--- IS 490 Intro to Data Science/HW9/HW9_extraction_52.sh
+++ IS 490 Intro to Data Science/HW9/HW9_extraction_52.sh
@@ -4,59 +4,26 @@
 
 # Question 2.
 
-
-cat temp.html | grep -A 8 "titleColumn"
-
-cat temp.html | grep -A 8 "titleColumn" | grep -oP "[0-9]\.[0-9]</strong>" | sed "s/<\/strong>//g" | wc -l
- # ratings, care for the 'P' on google cloud   --- . wc -l --> 250
-
-cat temp.html | grep -A 8 "titleColumn" | grep -oP "..[0-9].)" | sed "s/)//g" # year
-cat temp.html | grep -A 8 "titleColumn" | grep -oP "(...[0-9].)</span>" | sed "s/)<\/span>//g" # Year On Google Cloud!!
-
-cat temp.html | grep -A 1 ""titleColumn | grep "[0-9]" # Rank
-
-
-cat temp.html | grep -A 1 "titleColumn" | grep "[0-9]" | sed "s/ //g" | sed "s/\.//g" # ranking On google CLOUD
-
-
-
-
+# Save the commands that outputs starting from the <td class="titleColum"> and the number of lines (-A lineNumbers) behind it to two variables for further easier use
+htmldata_top8=`cat temp.html | grep -A 8 "titleColumn"| sed "s/\n//g"`
+htmldata_top1=`cat temp.html | grep -A 1 "titleColumn"| sed "s/\n//g"`
 
 # Ratings
-cat temp.html | grep -A 8 "titleColumn" | grep -oP "[0-9]\.[0-9]</strong>" | sed "s/<\/strong>//g" 
-# ratings, care for the 'P' on google cloud   --- . wc -l --> 250
+echo $htmldata_top8 | grep -oP "[0-9]\.[0-9]</strong>" | sed "s/<\/strong>//g" > ratings.txt
 
 # Year
-cat temp.html | grep -A 8 "titleColumn" | grep -oP "([0-9]+.)</span>" | sed "s/)<\/span>//g" # Year On Google Cloud!!
-
+echo $htmldata_top8 | grep -oP "[0-9]+\)" | sed "s/)//g" > year.txt
 
 # Ranking
-cat temp.html | grep -A 1 "titleColumn" | grep "[0-9]" | sed "s/ //g" | sed "s/\.//g" # ranking On google CLOUD
+echo $htmldata_top1 | grep -oP "[0-9]+" > rankings.txt
 
 # Title
-cat temp.html | grep -A 8 "titleColumn" | grep -oP ">(.*)</a>" | sed "s/>//g" | sed "s/<\/a//g"
+cat temp.html | grep -A 8 "titleColumn"| sed "s/\n//g" | grep -oP ">(.*)</a>" | sed "s/>//g" | sed "s/<\/a//g" | sed -e 's/^\|$/"/g' > title.txt
 
 
-cat temp.html | grep -A 8 "titleColumn" | grep -oP "[0-9]\.[0-9]</strong>" | sed "s/<\/strong>//g"  > ratings.txt
-cat temp.html | grep -A 8 "titleColumn" | grep -oP "([0-9]+.)</span>" | sed "s/)<\/span>//g" > year.txt
-cat temp.html | grep -A 1 "titleColumn" | grep "[0-9]" | sed "s/ //g" | sed "s/\.//g" > ranking.txt
-cat temp.html | grep -A 8 "titleColumn" | grep -oP ">(.*)</a>" | sed "s/>//g" | sed "s/<\/a//g" > title.txt
-sed -e 's/^\|$/"/g' title.txt > newTitle.txt
-
+# I wrote two commands with very small changes with regards to the commas in the title. Yet still keeping the result.csv as (comma seperated)
 # create the result.csv comma seperated file of our results
-paste -d "," ranking.txt newTitle.txt year.txt ratings.txt > result.csv
+paste -d "," rankings.txt title.txt year.txt ratings.txt > result.csv
 
-
-# 1. output the number of movies in result.csv that were released before a given year (the given year is an input argument) (4pts)
-awk -F "," -v param="$1" 'BEGIN{ count=0 }  { if ($(NF-1) < param ){count=count + 1} }  
-END{printf("\nQuestion 1. Output the number of movies in result.csv that were released before a given year\n\nNumber of movies released befor %s:  %s\n", param, count ) }' result.csv 
-
-# awk -F "," -v param="1980" 'BEGIN{  }  { if ($(NF-1) < param ){count[1]++} }  
-# END{printf("Number of movies released befor %s:  %s", param, count[1] ) }' result.csv
-
-# 2. output the title and the ratings of the movies from result.csv before the given year in (1.)
-awk -F "," -v param="$1" 'BEGIN{ printf("\nQuestion 2. output the title and the ratings of the movies from result.csv before the given year in (1.)\n%-40s  %-s\n\n", "TITLE", "RATINGS") }  { if ($(NF-1) < param ) {printf("%-40s  %-s\n"), $(NF-2) , $(NF)} }  
-END{}' result.csv  
-
-
-
+# create the result.csv with '~' seperated file of our results so when later called with Awk 'result_notcommaseperated.csv' file, it won't mess up the output with titles containing commas
+paste -d "~" rankings.txt title.txt year.txt ratings.txt > result_notcommaseperated.csv
